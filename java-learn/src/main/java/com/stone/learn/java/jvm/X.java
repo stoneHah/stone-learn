ac3c6cc58c5a908bbeb65ba7e8d27fade1441850
qun.zheng <zhengqun@youedata.com>
2019-01-18 21:07:22 +0800

    add spring learn moduler

---
+++ java-learn/src/main/java/com/stone/learn/java/jvm/X.java
@@ -0,0 +1,19 @@
+package com.stone.learn.java.jvm;
+
+/**
+ * ${DESCRIPTION}
+ *
+ * @author qun.zheng
+ * @create 2018/12/14
+ **/
+public class X {
+    public void foo() {
+        bar();
+    }
+
+    public void bar() { }
+
+    public static void main(String[] args) {
+
+    }
+}
